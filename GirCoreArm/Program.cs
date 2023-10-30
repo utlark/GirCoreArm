b1b9b5d5714df74965db2c17e891b84922993533
Yuriy Zubkov <utlark@ya.ru>
2023-10-30 15:40:11 +0500

    Попытка запустить Gercore.Gst-1.0 на linux arm

---
+++ GirCoreArm/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Gst;
+
+var pipelineString = args.Length >= 1 && !string.IsNullOrEmpty(args[0]) ? args[0] : "playbin uri=playbin uri=file:///opt/Costa_Rica_HD_25Fps.avi";
+
+Module.Initialize();
+Console.WriteLine($"Playing: {pipelineString}");
+
+Application.Init();
+var pipeline = Functions.ParseLaunch(pipelineString);
+pipeline.SetState(State.Playing);
+pipeline.GetBus()!.WaitForEndOrError();
+pipeline.SetState(State.Null);
